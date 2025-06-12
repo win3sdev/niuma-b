2fab35c8bbaf7c79aee7b6ad41faeb92fba39acd
win <win3dev@win3.com>
2025-06-11 20:39:03 -0700

    fix-review

--- middleware.ts
+++ middleware.ts
@@ -1,39 +1,27 @@
 import { NextRequest, NextResponse } from "next/server";
 import { getToken } from "next-auth/jwt";
 import { NextRequestWithAuth } from "next-auth/middleware";
-import { optimizePageRuntime, handleStaticJs } from "@/lib/middlewareHandlers";
 
 export default async function middleware(request: NextRequestWithAuth) {
-  // console.log(request);
-  const pathname = request.nextUrl.pathname;
-
   const token = await getToken({ req: request });
   const isAuthenticated = !!token;
 
-  const pageResponse = await optimizePageRuntime(request);
-  if (pageResponse) return pageResponse;
-
-  const staticResponse = handleStaticJs(pathname);
-  if (staticResponse) return staticResponse;
-
   // Public paths that don't require authentication
-  const isPublicPath = request.nextUrl.pathname === "/login";
+  const isPublicPath = request.nextUrl.pathname === '/login';
 
   // Redirect authenticated users away from public paths
   if (isAuthenticated && isPublicPath) {
-    return NextResponse.redirect(new URL("/dashboard", request.url));
+    return NextResponse.redirect(new URL('/dashboard', request.url));
   }
 
   // Redirect unauthenticated users to login page
   if (!isAuthenticated && !isPublicPath) {
-    return NextResponse.redirect(new URL("/login", request.url));
+    return NextResponse.redirect(new URL('/login', request.url));
   }
-
-  
 
   return NextResponse.next();
 }
 
 export const config = {
-  matcher: ["/", "/dashboard/:path*", "/login", "/_next/static/:path*",],
+  matcher: ["/", "/dashboard/:path*", "/login"],
 };
